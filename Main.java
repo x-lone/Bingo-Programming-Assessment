ac7defd6639723f0fd77a3746f69342de0ade605
x-lone <lonewolfx8749@my.uwstout.edu>
2025-02-18 22:39:01 -0600

    Added Author Mention in Main.java

--- Main.java
+++ Main.java
@@ -1,9 +1,13 @@
+//   Author: Xyrus Lonewolf
+//  Started: Feb 3, 2025
+// Finished: Feb 18, 2025
+
 import Bingo.GameHander;
 
 public class Main {
     public static void main(String[] args) {
         GameHander game = new GameHander();
-        
+
         if (game.createCardsFromTxt("BingoCards.txt")) {
             game.run();
         } else {
