64715d3170f96a1eadfa66be791149eb9f6bba0d
x-lone <lonewolfx8749@my.uwstout.edu>
2025-03-22 17:23:01 -0500

    Added P2 test case.

--- Bingo/PatternTest.java
+++ Bingo/PatternTest.java
@@ -7,7 +7,7 @@ import org.junit.Test;
 
 public class PatternTest {
     @Test
-    public void rowPattern() {
+    public void P1() {
         Pattern pattern = new Pattern("Row Pattern");
         CardHandler card = new CardHandler("card1", "3,22,32,51,66,6,19,44,60,61,7,27,45,58,72,15,16,48,46,75,1,30,31,52,63");
 
@@ -16,6 +16,19 @@ public class PatternTest {
         card.stampLocation("NB");
         card.stampLocation("GB");
         card.stampLocation("OB");
+        
+        assertEquals(true, pattern.checkCard(card));
+    }
+    @Test
+    public void P2() {
+        Pattern pattern = new Pattern("Row Pattern");
+        CardHandler card = new CardHandler("card1", "3,22,32,51,66,6,19,44,60,61,7,27,45,58,72,15,16,48,46,75,1,30,31,52,63");
+
+        card.stampLocation("BN");
+        card.stampLocation("IN");
+        card.stampLocation("NN");
+        card.stampLocation("GN");
+        card.stampLocation("ON");
         
         assertEquals(true, pattern.checkCard(card));
     }
