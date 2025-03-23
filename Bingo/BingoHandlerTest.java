23f57f905cecda2264071079a71ac23a64689fbf
x-lone <lonewolfx8749@my.uwstout.edu>
2025-03-23 01:13:24 -0500

    Finished BM5 test case by adding stamped spaces and fixing the added patterns by removing one of the two Column Patterns with a Diagonal Pattern.

--- Bingo/BingoHandlerTest.java
+++ Bingo/BingoHandlerTest.java
@@ -74,9 +74,13 @@ public class BingoHandlerTest {
         bingoHandler.createCardsFromTxt("BingoCards.txt");
         bingoHandler.addPattern("Row Pattern");
         bingoHandler.addPattern("Column Pattern");
-        bingoHandler.addPattern("Column Pattern");
+        bingoHandler.addPattern("Diagonal Pattern");
 
-
+        for (int y = 0; y < 5; y++) {
+            for (int x = 0; x < 5; x++) {
+                bingoHandler.allCards[4].stampLocation(""+"BINGO".charAt(x)+"BINGO".charAt(y));
+            }
+        }
 
         assertEquals(12, bingoHandler.countBingos(bingoHandler.allCards[4]));
     }
