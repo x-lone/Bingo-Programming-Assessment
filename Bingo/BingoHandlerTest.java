aa26d6398d28add855f048d7c88648a633059697
x-lone <lonewolfx8749@my.uwstout.edu>
2025-03-23 00:46:24 -0500

    Finished BM3 test case by adding the stamp locations.

--- Bingo/BingoHandlerTest.java
+++ Bingo/BingoHandlerTest.java
@@ -39,7 +39,14 @@ public class BingoHandlerTest {
         bingoHandler.createCardsFromTxt("BingoCards.txt");
         bingoHandler.addPattern("Row Pattern");
 
-
+        for (int y = 0; y < 5; y+=2) {
+            for (int x = 0; x < 5; x++) {
+                bingoHandler.allCards[0].stampLocation(""+"BINGO".charAt(x)+"BINGO".charAt(y));
+            }
+        }
+        for (int y = 0; y < 5; y++) {
+            bingoHandler.allCards[0].stampLocation("O"+"BINGO".charAt(y));
+        }
 
         assertEquals(3, bingoHandler.countBingos(bingoHandler.allCards[0]));
     }
