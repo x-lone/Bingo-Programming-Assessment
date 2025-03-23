3eb244d131465a9a9812d498f268fd0d383e8933
x-lone <lonewolfx8749@my.uwstout.edu>
2025-03-23 14:43:40 -0500

    Added default Patterns so that the acutal bingo game can be played.

--- Main.java
+++ Main.java
@@ -9,6 +9,9 @@ public class Main {
         BingoHandler game = new BingoHandler();
 
         if (game.createCardsFromTxt("BingoCards.txt")) {
+            game.addPattern("Row Pattern");
+            game.addPattern("Column Pattern");
+            game.addPattern("Diagonal Pattern");
             game.run();
         } else {
             System.out.println("There was an issue with the input path. Perhaps is doesn't exist or it's formated incorrectly.");
