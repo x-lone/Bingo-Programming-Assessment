fabd006737a0753fbd70dd46f882cb290e225a30
x-lone <lonewolfx8749@my.uwstout.edu>
2025-03-22 20:22:04 -0500

    Finished P16 unit test.

--- Bingo/PatternTest.java
+++ Bingo/PatternTest.java
@@ -210,7 +210,14 @@ public class PatternTest {
         Pattern pattern = new Pattern(customPattern);
         CardHandler card = new CardHandler("card8", "13,20,35,58,69,3,22,39,54,74,1,17,34,55,75,2,25,45,57,73,11,21,31,52,70");
 
-
+        for (int y = 0; y < 5; y++) {
+            card.stampLocation(""+"BINGO".charAt(y)+"B");
+            card.stampLocation(""+"BINGO".charAt(y)+"O");
+        }
+        for (int y = 1; y < 4; y++) {
+            card.stampLocation("B"+"BINGO".charAt(y));
+            card.stampLocation("O"+"BINGO".charAt(y));
+        }
         
         assertEquals(true, pattern.checkCard(card));
     }
